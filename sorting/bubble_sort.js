1fc0f9475bf2da06f535b965adeb2f4ade8c56f0
Atharv Karole <atharvkarole@Atharvs-Mac.local>
2026-08-02 13:04:12 +0530

    Fix : optimization in Bubble sort

--- sorting/bubble_sort.js
+++ sorting/bubble_sort.js
@@ -1,16 +1,23 @@
-let arr = [12,5,6,2,44,9];
+let arr = [1,2,3,4,5,6];
 
 function bubbleSort(arr){
     let n = arr.length;
     for(let i=0;i<n-1;i++){
+        let flag = false;
         for(let j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
                 let temp = arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
+                flag = true;
             }
         }
+        if(!flag) break; // for optimization 
     }
     return arr;
 }
 console.log(bubbleSort(arr));   
+
+
+// The bubble sort is about checking the two value and swap.  
+// Tc - o(n^2) Sc-o(1)
